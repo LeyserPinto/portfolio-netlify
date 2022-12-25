93a3969359ff9a461611643973adcfec8bf79515
Leyser Pinto <leyserpinto3@gmail.com>
2022-12-25 18:54:16 -0300

    add redirects file

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />
   },
   {
-    path: "habilidades",
+    path: "/habilidades",
     element: <Habilidades />,
     errorElement: <ErrorPage />
   },
